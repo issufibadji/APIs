6765e350e463e271e7d82f6bbe943ba7de60889d
trf/conibj <issufi.badji@tty2000.com.br>
2023-03-24 10:50:47 -0300

    subido arquivos

--- constalr-cnpj/app/WebService/Speedio.php
+++ constalr-cnpj/app/WebService/Speedio.php
@@ -6,7 +6,7 @@ namespace App\WebService;
 class Speedio{
 
     /**
-     * URL base da API DO sPEEDIO
+     * URL base da API do Speedio
      * @var string
      */
 
@@ -19,7 +19,55 @@ class Speedio{
       */
       public function consultarCNPJ($cnpj){
         
-        return $this->get('/buscarcnpj?cnpj');
+        //REMOVE OS CARACTERS INVÁLIDOS
+        $cnpj = PREG_REPLACE('/\D/', '', $cnpj);
+
+        
+        //RETORNA A EXCUÇÃ DA CONSULTA
+        return $this->get('/buscarcnpj?cnpj=' .$cnpj);
       }
       
+
+      /**
+       * Método responsável por executar a consulta nas APIs do Speedio
+       * @param string $resource //retorana string 
+       * @return array //retorna array
+       * 
+       */
+      public function get($resource){
+        //ENDIPOINT COMPLETO DA API
+        $endpoint = self::URL_BASE.$resource;//concactenar nosso $resource
+
+       //CURL PARA EXCUTAR A NOSSA REQUESIÇÃO
+
+      // INICIA O CURL
+        $curl = curl_init();
+        
+        //CONFIGURAÇÕES DO CURL
+       // METODO/FUNC DE CURL PARA PASSAR ARRAY
+        curl_setopt_array($curl, [
+
+          CURLOPT_URL => $endpoint,// ENDEPOIT PARA ONDE ELE VAI ENVIAR ESTA REQUISIÇAO
+          CURLOPT_RETURNTRANSFER => true, //PARA RETORNA CONTEUDO E NÃO IMPRIMIR NA TELA
+          CURLOPT_CUSTOMREQUEST => 'GET' //NOSSA REQUISÃO GET
+        ]);
+
+
+         //EXCUTAR A REQUISIÇÃO
+         $response = curl_exec($curl);
+
+         //FECHA A CONEXÃO
+         curl_close($curl);
+
+
+         //RESPONSE A ARRAY
+         $responseArray = json_decode($response,true); 
+
+        //RETURNO ARRAY COM DADOS
+         return is_array($responseArray)? $responseArray : [];//SE FOR ARRAY RESPONDE SE NÃO RETORNA VAZIO
+
+        
+
+
+      }
     }
